60642344c7ec9512f70fd323c91183ba3bcc0777
aminul-islam6911 <84971228+aminul-islam6911@users.noreply.github.com>
2024-12-12 01:11:02 +0600

    authentication, clerk, middleware

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,19 @@
+import { UserButton } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs/server";
 import Image from "next/image";
 
-const Navbar = () => {
+const Navbar = async () => {
+  const user = await currentUser();
   return (
     <div className="flex items-center justify-between p-4">
       {/* Search Bar */}
       <div className="hidden md:flex items-center gap-2 text-xs rounded-full ring-[1.5px] ring-gray-300 px-2">
         <Image src="/search.png" alt="" width={14} height={14} />
-        <input type="text" className="w-[200px] p-2 bg-transparent outline-none" placeholder="Search..." />
+        <input
+          type="text"
+          className="w-[200px] p-2 bg-transparent outline-none"
+          placeholder="Search..."
+        />
       </div>
       {/* Icons and Users*/}
       <div className="flex items-center gap-6 w-full justify-end">
@@ -15,13 +22,18 @@ const Navbar = () => {
         </div>
         <div className="rounded-full w-7 h-7 flex items-center justify-center cursor-pointer relative">
           <Image src="/announcement.png" alt="" width={20} height={20} />
-          <div className="absolute -top-3 -right-2.5 w-5 h-5 flex items-center justify-center bg-purple-500 text-white text-sm rounded-full">1</div>
+          <div className="absolute -top-3 -right-2.5 w-5 h-5 flex items-center justify-center bg-purple-500 text-white text-sm rounded-full">
+            1
+          </div>
         </div>
         <div className="flex flex-col">
-            <span className="text-xs leading-3 font-medium">Aminul Islam</span>
-            <span className="text-[10px] text-gray-500 text-right">Admin</span>
+          <span className="text-xs leading-3 font-medium">Aminul Islam</span>
+          <span className="text-[10px] text-gray-500 text-right">
+            {user?.publicMetadata.role as string}
+          </span>
         </div>
-        <Image src="/avatar.png" alt="" width={36} height={36} className="rounded-full cursor-pointer" />
+        {/* <Image src="/avatar.png" alt="" width={36} height={36} className="rounded-full cursor-pointer" /> */}
+        <UserButton />
       </div>
     </div>
   );
